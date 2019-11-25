8372c61b1695c7de9c26bb7d48ea9432a0c71691
Greg <greg.rabone@gmail.com>
2019-11-25 13:32:27 +1300

    Javascript and some text

---
+++ public/js/contactPage.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() { 
+  this.document.getElementById('navlinkOne').className ='siimple-navbar-item'; 
+  this.document.getElementById('navlinkFour').className = 'siimple-navbar-item siimple-nav-item--selected siimple--color-success'; 
+});
