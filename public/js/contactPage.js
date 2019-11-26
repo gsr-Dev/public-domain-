95ede69c25db380382037764832a89476c960048
Greg <greg.rabone@gmail.com>
2019-11-27 12:59:53 +1300

    New changes

--- public/js/contactPage.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', function() { 
-  this.document.getElementById('navlinkOne').className ='siimple-navbar-item'; 
-  this.document.getElementById('navlinkFour').className = 'siimple-navbar-item siimple-nav-item--selected siimple--color-success'; 
-});
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,20 @@
-const express = require("express"); 
+const express = require('express'); 
 const router = express.Router(); 
 
-router.get("/", function(req, res) {
-    res.render("home"); 
+router.get('/', function(req, res) {
+    res.render('home'); 
 }); 
 
-router.get("/contact", function(req, res) {
-    res.render("contact");
-});
+router.get('/contact', function(req, res) {
+    res.render('contact');
+}); 
+
+router.get('/blog', function(req, res) {
+    res.render('blog');
+}); 
+
+router.get('/location', function(req, res) {
+    res.render('location'); 
+}); 
 
 module.exports = router; 
